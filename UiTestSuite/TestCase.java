98175e8a0b016f24f75c6afafca0015fe918d9b6
cbellom <cbellom@unal.edu.co>
2014-11-10 00:47:32 -0500

    Semi Final version

--- UiTestSuite/TestCase.java
+++ UiTestSuite/TestCase.java
@@ -18,7 +18,19 @@ import com.android.uiautomator.testrunner.*;
 public class TestCase extends UiAutomatorTestCase {
 
 	private static String TAG = "TestCase";
+	private static int SIZE_TYPES = 18;
 
+	private String[] types = { "android.widget.FrameLayout",
+			"android.widget.LinearLayout", "android.widget.RelativeLayout",
+			"android.view.View", "android.widget.GridView",
+			"android.widget.ListView", "android.widget.ImageView",
+			"android.widget.TextView", "android.widget.Switch",
+			"android.widget.Button", "android.widget.DatePicker",
+			"android.widget.CheckBox", "android.widget.EditText",
+			"android.widget.RadioButton", "android.widget.RadioGroup",
+			"android.widget.ToggleButton", "android.widget.Spinner",
+			"android.widget.TimePicker", "android.support.v4.view.ViewPager",
+			"android.widget.TableRow" };
 	private String propertiesPath = "/UiTest/properties.txt";
 	private String TreePath = "/UiTest/Tree.txt";
 	private String errorPath = "/UiTest/error.txt";
@@ -30,7 +42,7 @@ public class TestCase extends UiAutomatorTestCase {
 	private String keyStringActivity = "";
 
 	public void test() {
-		try{ 
+		try {
 			readProperties();
 			appName = args[0];
 			packageName = args[1];
@@ -41,24 +53,20 @@ public class TestCase extends UiAutomatorTestCase {
 			findObjectsFromContainer(element);
 			evaluateState();
 			writeTree();
-			// System.out.println("------------- Tree :D "+Tree);
-			// Validate that the package name is the expected one
-			// UiObject settingsValidation = new UiObject(
-			// new UiSelector().packageName(packageName));
-	
-			// assertTrue("Unable to detect Settings", settingsValidation.exists());
-			// // CODE:END
-		}catch (Exception e) {
-			writeError("ERROR: test: "+e.getMessage());
+
+		} catch (Exception e) {
+			writeTree();
+			writeError("ERROR: test: " + e.getMessage());
 		}
 	}
 
 	public String getActualActivity() throws UiObjectNotFoundException {
-		UiObject o = new UiObject(new UiSelector().className("android.widget.TextView"));
-		if(o.exists()){
+		UiObject o = new UiObject(
+				new UiSelector().className("android.widget.TextView"));
+		if (o.exists()) {
 			keyStringActivity = o.getText();
 		}
-		return getUiDevice().getCurrentPackageName()+keyStringActivity;
+		return getUiDevice().getCurrentPackageName() + keyStringActivity;
 	}
 
 	public boolean isAnyEventAvaiable(UiObject object)
@@ -83,7 +91,7 @@ public class TestCase extends UiAutomatorTestCase {
 	}
 
 	public Node addNode(UiObject object) throws UiObjectNotFoundException {
-		if (object!=null && isAnyEventAvaiable(object)) {
+		if (object != null && isAnyEventAvaiable(object)) {
 			Node Node = new Node(actual, object.getChildCount() + "",
 					getActualActivity(), object);
 			actual.add(Node);
@@ -104,303 +112,57 @@ public class TestCase extends UiAutomatorTestCase {
 			element = new UiObject(selector);
 		} catch (Exception e) {
 			System.out.println("---ERROR: getGeneralElement");
-			writeError("ERROR: getGeneralElement: "+e.getMessage());
+			writeError("ERROR: getGeneralElement: " + e.getMessage());
 			element = null;
 		}
 		return element;
 	}
-	
-	public void findFrameLayout(UiObject container)
-			throws UiObjectNotFoundException {
-		UiObject frameLayout = container.getChild(new UiSelector()
-				.className(android.widget.FrameLayout.class.getName()));
-		if (frameLayout.exists()) {
-			System.out.println("---frameLayout");
-			findObjectsFromContainer(frameLayout);
-			addNode(frameLayout);
-		}
-	}
-
-	public void findLinearLayout(UiObject container)
-			throws UiObjectNotFoundException {
-		UiObject linearLayout = container.getChild(new UiSelector()
-				.className(android.widget.LinearLayout.class.getName()));
-		if (linearLayout.exists()) {
-			System.out.println("---linearLayout");
-			findObjectsFromContainer(linearLayout);
-			addNode(linearLayout);
-		}
-	}
-
-	public void findRelativeLayout(UiObject container)
-			throws UiObjectNotFoundException {
-		UiObject relativeLayout = container.getChild(new UiSelector()
-				.className(android.widget.RelativeLayout.class.getName()));
-		if (relativeLayout.exists()) {
-			System.out.println("---relativeLayout");
-			findObjectsFromContainer(relativeLayout);
-			addNode(relativeLayout);
-		}
-	}
-
-	public void findView(UiObject container) throws UiObjectNotFoundException {
-		UiObject view = container.getChild(new UiSelector()
-				.className(android.view.View.class.getName()));
-		if (view.exists()) {
-			System.out.println("---view");
-			findObjectsFromContainer(view);
-			addNode(view);
-		}
-	}
-
-	public void findGridView(UiObject container)
-			throws UiObjectNotFoundException {
-		UiObject gridView = container.getChild(new UiSelector()
-				.className(android.widget.GridView.class.getName()));
-		if (gridView.exists()) {
-			System.out.println("---gridView");
-			findObjectsFromContainer(gridView);
-			addNode(gridView);
-		}
-	}
-
-	public void findListView(UiObject container)
-			throws UiObjectNotFoundException {
-		UiObject listView = container.getChild(new UiSelector()
-				.className(android.widget.ListView.class.getName()));
-		if (listView.exists()) {
-			System.out.println("---listView");
-			findObjectsFromContainer(listView);
-			addNode(listView);
-		}
-	}
-
-	public void findSwitch(UiObject container) throws UiObjectNotFoundException {
-		UiObject switchObject = container.getChild(new UiSelector()
-				.className(android.widget.Switch.class.getName()));
-		if (switchObject.exists()) {
-			System.out.println("---switchObject");
-			findObjectsFromContainer(switchObject);
-			addNode(switchObject);
-		}
-	}
-
-	public void findButton(UiObject container) throws UiObjectNotFoundException {
-		UiObject button = container.getChild(new UiSelector()
-				.className(android.widget.Button.class.getName()));
-		if (button.exists()) {
-			System.out.println("---button");
-			findObjectsFromContainer(button);
-			addNode(button);
-		}
-	}
-
-	public void findDatePicker(UiObject container)
-			throws UiObjectNotFoundException {
-		UiObject datePicker = container.getChild(new UiSelector()
-				.className(android.widget.DatePicker.class.getName()));
-		if (datePicker.exists()) {
-			System.out.println("---datePicker");
-			findObjectsFromContainer(datePicker);
-			addNode(datePicker);
-		}
-	}
-
-	public void findCheckBox(UiObject container)
-			throws UiObjectNotFoundException {
-		UiObject checkBox = container.getChild(new UiSelector()
-				.className(android.widget.CheckBox.class.getName()));
-		if (checkBox.exists()) {
-			System.out.println("---CheckBox");
-			findObjectsFromContainer(checkBox);
-			addNode(checkBox);
-		}
-	}
-
-	public void findEditText(UiObject container)
-			throws UiObjectNotFoundException {
-		UiObject editText = container.getChild(new UiSelector()
-				.className(android.widget.EditText.class.getName()));
-		if (editText.exists()) {
-			System.out.println("---Frame");
-			findObjectsFromContainer(editText);
-			addNode(editText);
-		}
-	}
-
-	public void findRadioButton(UiObject container)
-			throws UiObjectNotFoundException {
-		UiObject radioButton = container.getChild(new UiSelector()
-				.className(android.widget.RadioButton.class.getName()));
-		if (radioButton.exists()) {
-			System.out.println("---radioButton");
-			findObjectsFromContainer(radioButton);
-			addNode(radioButton);
-		}
-	}
-
-	public void findRadioGroup(UiObject container)
-			throws UiObjectNotFoundException {
-		UiObject radioGroup = container.getChild(new UiSelector()
-				.className(android.widget.RadioGroup.class.getName()));
-		if (radioGroup.exists()) {
-			System.out.println("---radioGroup");
-			findObjectsFromContainer(radioGroup);
-			addNode(radioGroup);
-		}
-	}
-
-	public void findToggleButton(UiObject container)
-			throws UiObjectNotFoundException {
-		UiObject toggleButton = container.getChild(new UiSelector()
-				.className(android.widget.ToggleButton.class.getName()));
-		if (toggleButton.exists()) {
-			System.out.println("---toggleButton");
-			findObjectsFromContainer(toggleButton);
-			addNode(toggleButton);
-		}
-	}
-
-	public void findSpinner(UiObject container)
-			throws UiObjectNotFoundException {
-		UiObject spinner = container.getChild(new UiSelector()
-				.className(android.widget.Spinner.class.getName()));
-		if (spinner.exists()) {
-			System.out.println("---spinner");
-			findObjectsFromContainer(spinner);
-			addNode(spinner);
-		}
-	}
-
-	public void findTimePicker(UiObject container)
-			throws UiObjectNotFoundException {
-		UiObject timePicker = container.getChild(new UiSelector()
-				.className(android.widget.TimePicker.class.getName()));
-		if (timePicker.exists()) {
-			System.out.println("---timePicker");
-			findObjectsFromContainer(timePicker);
-			addNode(timePicker);
-		}
-	}
-
-	public void findImageView(UiObject container)
-			throws UiObjectNotFoundException {
-		UiObject imageView = container.getChild(new UiSelector()
-				.className(android.widget.ImageView.class.getName()));
-		if (imageView.exists()) {
-			System.out.println("---imageView");
-			findObjectsFromContainer(imageView);
-			addNode(imageView);
-		}
-	}
-
-	public void findTextView(UiObject container)
-			throws UiObjectNotFoundException {
-		UiObject textView = container.getChild(new UiSelector()
-				.className(android.widget.TextView.class.getName()));
-		if (textView.exists()) {
-			System.out.println("---textView");
-			findObjectsFromContainer(textView);
-			addNode(textView);
-		}
-	}
 
 	public void findObjectsFromContainer(UiObject container)
 			throws UiObjectNotFoundException {
-		System.out.println("-Buscando todos");
-		try {
-			findFrameLayout(container);
-		} catch (Exception e) {
-			System.out.println("-Error findFrameLayout");
-		}
-		try {
-			findLinearLayout(container);
-		} catch (Exception e) {
-			System.out.println("-Error findLinearLayout");
-		}
-		try {
-			findRelativeLayout(container);
-		} catch (Exception e) {
-			System.out.println("-Error findRelativeLayout");
-		}
-		try {
-			findView(container);
-		} catch (Exception e) {
-			System.out.println("-Error findView");
-		}
-		try {
-			findListView(container);
-		} catch (Exception e) {
-			System.out.println("-Error findListView");
-		}
-		try {
-			findGridView(container);
-		} catch (Exception e) {
-			System.out.println("-Error findGridView");
-		}
-		try {
-			findButton(container);
-		} catch (Exception e) {
-			System.out.println("-Error findButton");
-		}
-		try {
-			findSwitch(container);
-		} catch (Exception e) {
-			System.out.println("-Error findSwitch");
-		}
-		try {
-			findCheckBox(container);
-		} catch (Exception e) {
-			System.out.println("-Error findCheckBox");
-		}
-		try {
-			findEditText(container);
-		} catch (Exception e) {
-			System.out.println("-Error findEditText");
-		}
-		try {
-			findTextView(container);
-		} catch (Exception e) {
-			System.out.println("-Error findTextView");
-		}
-		try {
-			findSpinner(container);
-		} catch (Exception e) {
-			System.out.println("-Error findSpinner");
-		}
-		try {
-			findRadioButton(container);
-		} catch (Exception e) {
-			System.out.println("-Error findRadioButton");
-		}
-		try {
-			findRadioGroup(container);
-		} catch (Exception e) {
-			System.out.println("-Error findRadioGroup");
-		}
-		try {
-			findToggleButton(container);
-		} catch (Exception e) {
-			System.out.println("-Error findToggleButton");
-		}
-		try {
-			findTimePicker(container);
-		} catch (Exception e) {
-			System.out.println("-Error findTimePicker");
-		}
-		try {
-			findDatePicker(container);
-		} catch (Exception e) {
-			System.out.println("-Error findDatePicker");
+		System.out.println("-Buscando todos en " + getActualActivity());
+		for (int i = 0; i < SIZE_TYPES; i++) {
+			UiSelector viewSelector = new UiSelector().className(types[i]);
+			if (viewSelector != null) {
+				// get the last child
+				UiCollection children = new UiCollection(viewSelector);
+				if (children.exists()) {
+					int childCount = children.getChildCount();
+					// UiObject lastChild = children.getChildByInstance(new
+					// UiSelector().className("android.view.View"), childCount -
+					// 1);
+					System.out.println("-----------------child count papa: "
+							+ childCount);
+					for (int j = 0; j < SIZE_TYPES; j++) {
+						for (int k = 0; k < childCount; k++) {
+							// UiObject object = collect.getChild(new
+							// UiSelector().index(i));
+							UiObject object = children.getChildByInstance(
+									new UiSelector().className(types[j]), k);
+							if (object.exists()) {
+								addNode(object);
+								System.out.println("-----------------objeto: "
+										+ object.getText());
+								System.out.println("-----------------------: "
+										+ object.getChildCount());
+								System.out.println("-----------------------: "
+										+ object.getBounds());
+								// System.out.println("-----------------------: "+object.getClassName());
+							}
+						}
+					}
+				}
+			}
 		}
 	}
 
 	public void evaluateState() throws UiObjectNotFoundException {
-		System.out.println("------------ evaluate Node " + actual.getId()
-				+ " state " + actual.getState());
+		System.out.println("------------ evaluate Node " + actual.getId() + " "
+				+ actual.getObject().getText() + " with" + " state "
+				+ actual.getState());
 		if (actual.getState().equals(NodeState.PASS)) {
 			actual = actual.getDad();
-			if(actual==null){
+			if (actual == null) {
 				getUiDevice().pressBack();
 				return;
 			}
@@ -435,6 +197,8 @@ public class TestCase extends UiAutomatorTestCase {
 			}
 
 		} else if (actual.getState().equals(NodeState.VISITED)) {
+			System.out.println("XX" + actual.isAnyEventAvaiable() + " "
+					+ !isActualRoot());
 			if (actual.isAnyEventAvaiable() && !isActualRoot()) {
 				String activity = executeEvents();
 				if (!activity.equals(actual.getActivity())) {
@@ -446,10 +210,11 @@ public class TestCase extends UiAutomatorTestCase {
 					}
 				}
 			} else {
-				if(actual.getChilds()!=null){
+				if (actual.getChilds() != null) {
 					for (Node n : actual.getChilds()) {
 						if (n.getState().equals(NodeState.NONE)
 								|| n.getState().equals(NodeState.VISITED)) {
+							System.out.println("666");
 							actual = n;
 							evaluateState();
 							return;
@@ -458,22 +223,15 @@ public class TestCase extends UiAutomatorTestCase {
 				}
 				if (!isActualRoot()) {
 					actual.setState(NodeState.PASS);
-					if (!actual.getDad().getActivity()
-							.equals(actual.getActivity())) {
-						getUiDevice().pressBack();
-					}
 				} else {
-					System.out.println("1");
-					if(actual!=null){
-						System.out.println("2");
+					if (actual != null) {
 						actual.setState(NodeState.PASS);
-					}else{
-						System.out.println("3");
+					} else {
 						getUiDevice().pressBack();
 						return;
 					}
 					System.out.println("4");
-					
+
 				}
 			}
 		}
@@ -534,6 +292,7 @@ public class TestCase extends UiAutomatorTestCase {
 				text.append(line);
 				text.append('\n');
 				line = line.replace(" ", "");
+				line = line.replace("%20%", " ");
 				args[index] = line.substring(line.indexOf("=") + 1,
 						line.length());
 				index++;
@@ -783,14 +542,21 @@ class Node {
 	}
 
 	public boolean isAnyEventAvaiable() {
-		return clickable || scrollable || checkable || longClickable;
+		return clickable || checkable || longClickable;
 	}
 
 	public String toString() {
 		String text = "";
-		text += "id: " + id + " \n";
-		for (Node n : childs) {
-			text += "\t" + n.toString();
+		try {
+			text += "id: " + id + " - text: " + object.getText() + " \n";
+			if (childs != null) {
+				for (Node nodo : childs) {
+					text += "	" + nodo.toString();
+				}
+			}
+		} catch (UiObjectNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		return text;
 	}
